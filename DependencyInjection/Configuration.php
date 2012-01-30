7b953780caa90aa9438c16e984f58e9882e9c5ed
Markus Weiland <markusw@brandmypic.com>
2012-01-29 19:28:04 -0500

    Fixed configuration loading to work with Symfony 2.0 production release.
    Refactored configuration loading to use Configuration class.
    
    Simplified extension definition by using Symfony's Extension base class instead of ExtensionInterface.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tirna\KissmetricsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('tirna_kissmetrics', 'array');
+
+        $rootNode
+            ->children()
+            ->scalarNode('apiKey')->defaultValue('')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
--- DependencyInjection/TirnaKissmetricsExtension.php
+++ DependencyInjection/TirnaKissmetricsExtension.php
@@ -2,7 +2,9 @@
 
 namespace Tirna\KissmetricsBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -12,27 +14,17 @@ class TirnaKissmetricsExtension extends Extension {
 		'kissmetrics_tracker'   => 'tracker.xml',
 	);
 
-	public function trackerLoad($config, ContainerBuilder $container) {
-		if (!$container->hasDefinition('kissmetrics.tracker')) {
-			$loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
-			$loader->load($this->resources['kissmetrics_tracker']);
-		}
-		if (isset($config['config'])) {
-			$container->setParameter('kissmetrics.tracker.config', $config['config']);
-		}
-		return $container;
-	}
-
-	public function getAlias() {
-		return 'tirna_kissmetrics';
-	}
-
-	public function getNamespace() {
-		return 'http://www.symfony-project.org/schema/dic/kissmetrics';
-	}
-
-	public function getXsdValidationBasePath() {
-		return __DIR__.'/../Resources/config/schema';
-	}
-
+    public function load(array $configs, ContainerBuilder $container) {
+    	$config = $this->processConfiguration(new Configuration(), $configs);
+    	
+        if (!$container->hasDefinition('tirna_kissmetrics')) {
+    		$loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    		$loader->load($this->resources['kissmetrics_tracker']);
+    	}
+    	
+    	if (isset($config['config'])) {
+    	    $container->setParameter('kissmetrics.tracker.config', $config['config']);
+    	}
+    	return $container;
+    }
 }
